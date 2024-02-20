f51d47598bf7a18915bf35ba3954f2df4219225a
Tanzim <smubarra@purdue.edu>
2024-02-20 18:51:30 -0500

    added audio conversion code

---
+++ AudioConversion_RQ3.py
@@ -0,0 +1,29 @@
+from pydub import AudioSegment
+
+from pydub.playback import play
+import moviepy.editor as mp
+
+import os
+
+def convertAudio(filename):
+    print(filename + "\n")
+    my_clip = mp.VideoFileClip(directory + '\\' + filename)
+    
+    out_filename = filename.replace('.mp4','.wav')
+    
+    my_clip.audio.write_audiofile(directory + '\\' + out_filename)
+
+
+directory = 'videos_RQ3'
+
+# list to store files
+res = []
+# Iterate directory
+for file in os.listdir(directory):
+    # check only text files
+    if file.endswith('.mp4'):
+        res.append(file)
+        
+# print(res)
+for i in range(0,len(res)):
+    convertAudio(res[i])
